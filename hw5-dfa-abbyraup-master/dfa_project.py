ed54eb52a78b0b0e2f8532d8ed1020b2f2d6c062
abbyraup <ar6770a@student.american.edu>
2019-10-22 11:05:16 -0400

    pghg

---
+++ hw5-dfa-abbyraup-master/dfa_project.py
@@ -0,0 +1,44 @@
+def my_dfa(s):
+    L=list(s)
+    if len(L) < 1:
+        return ('q0', False)
+    hist=['q0']
+    while len(L)>0:
+        current_node=hist[-1]
+        current_character = L[0]
+        if current_node=='q0' and current_character=='0':
+            hist.append('q1')
+        elif current_node=='q0' and current_character=='1':
+            hist.append('q2')
+        elif current_node == 'q1' and current_character == '0':
+            hist.append('q3')
+        elif current_node == 'q1' and current_character == '1':
+            hist.append('q2')
+        elif current_node == 'q2' and current_character == '0':
+            hist.append('q1')
+        elif current_node == 'q2' and current_character == '1':
+            hist.append('q4')
+        elif current_node == 'q3' and current_character == '0':
+            hist.append('q3')
+        elif current_node == 'q3' and current_character == '1':
+            hist.append('q2')
+        elif current_node == 'q4' and current_character == '0':
+            hist.append('q1')
+        elif current_node == 'q4' and current_character == '1':
+            hist.append('q4')
+        L.pop(0)
+    return(hist)
+
+"""
+
+
+        def fun():
+                if hist[-1]=='q3'or'q4':
+                return True
+                else:
+                return False
+        return (hist, fun())
+
+
+
+"""
